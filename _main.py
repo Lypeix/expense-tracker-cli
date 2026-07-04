5338f9b954177a0281a9de114df21fc7da2497fe
Lypeix <patrykkudla1010@gmail.com>
2026-07-04 16:50:28 +0200

    Update _main.py

--- _main.py
+++ _main.py
@@ -1,18 +1,40 @@
 from models import Expense, ExpenseTracker
+from utils import user_choice, get_float
+
+def create_expense_from_input():
+    name = input("Name: ")
+    amount = get_float("Amount: ")
+    category = input("Category: ")
+    date = input("Date: ")
+
+    return Expense(name, amount, category, date)
 
 def main():
     tracker = ExpenseTracker()
+    
+    while True:
+        action = user_choice("What would you like to do?"
+                    "\n1. Add expense"
+                    "\n2. Show expenses"
+                    "\n3. Show total spent amount"
+                    "\n4. Quit"
+                    "\n> ",
+                    ["1", "2", "3", "4"]
+                    )
+        
+        if action == "1":
+            expense = create_expense_from_input()
+            tracker.add_expense(expense)
 
-    expense_1 = Expense("Pizza", 30, "food", "2026-07-01")
-    expense_2 = Expense("Manga", 55, "entertainment", "2026-06-30")
+        elif action == "2":
+            for expense in tracker.expenses:
+                print(f"{expense.name} - {expense.amount} PLN - {expense.category} - {expense.date}")
 
-    tracker.add_expense(expense_1)
-    tracker.add_expense(expense_2)
+        elif action == "3":
+            print(f"Total spent: {tracker.get_total_spent()} PLN")
 
-    for expense in tracker.expenses:
-        print(f"{expense.name} - {expense.amount} PLN - {expense.category} - {expense.date}")
-
-    print(f"Total spent: {tracker.get_total_spent()} PLN")
+        elif action == "4":
+            break
 
 if __name__ == "__main__":
     main()
